749ff4b0e4e5be2994d2949be629f2ab3dde2af3
abelsouzacosta <abelsouzacostajunior@proton.me>
2024-11-14 05:25:35 -0300

    [refactor]: reorganizing the code to make it less autistic

--- krofi/main.py
+++ krofi/main.py
@@ -7,29 +7,24 @@ from time import sleep
 from urllib import parse
 from pyotp import TOTP
 
+# initializes rofi
 rofi_instance = Rofi()
 
-
-# Will copy the credential to the clipboard
-# then wait 10 seconds and clear the clipboard
-def copy_entry(credential_entry):
-    copy(credential_entry)
-    sleep(10)
-    copy("")
-
-
-# command to run in subprocess
+# this command will run in a subprocess
+# i couldn't make the python library for rofi execute
+# dmenu with the '-password' parameter, so this is a workaround
 command = ["rofi", "-dmenu", "-p", "Enter database password: ", "-password"]
 
-# ask for credentials
+# will get the database password
+# if any is given returns an error
 credential = run(command, capture_output=True, text=True).stdout.strip()
 if not credential:
     rofi_instance.exit_with_error("Please provide valid credentials")
 
-print(f"{credential}")
 # get the database through a environment variable
+# the environment variable KPDB should be a absolute path your .kbdx file
+# like /home/<youruser>/dir/dir1/Pass.kbdx
 database_path = getenv("KPDB")
-print(f"{database_path}")
 # open database and validate credentials
 try:
     keepass_database = PyKeePass(database_path, password=credential)
@@ -39,13 +34,15 @@ except Exception:
 # get all groups from the database
 groups = keepass_database.groups
 
+# grouping all the group names into a list
 group_options = [group.name for group in groups]
 
 index_group, key_group = rofi_instance.select("Groups", options=group_options)
 
 # get the selected group in the previous step
 selected_group = groups[index_group]
-# get all entries from the group
+
+# get all entries from the selected group
 entries_options = [entry.title for entry in selected_group.entries]
 
 index_entry, key_entry = rofi_instance.select(
@@ -56,8 +53,8 @@ index_entry, key_entry = rofi_instance.select(
     key3=("Alt+t", "Copy TOTP"),
 )
 
+# get the selected entry in the previous step
 selected_entry = selected_group.entries[index_entry]
-print(f"index entry: {index_entry}, key_entry: {key_entry}")
 
 
 # will get the secret of the otp uri attribute
@@ -77,6 +74,14 @@ def generate_otp_code(secret):
     totp = TOTP(secret)
     code = totp.now()
     return code
+
+
+# Will copy the credential to the clipboard
+# then wait 10 seconds and clear the clipboard
+def copy_entry(credential_entry):
+    copy(credential_entry)
+    sleep(10)
+    copy("")
 
 
 if key_entry == 1:
